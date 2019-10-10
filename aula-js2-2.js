5317dce700cb68eb795d7a69b731825ee97eb3e8
Paula <pfr.blackcat@gmail.com>
2019-10-10 19:52:19 -0300

    subindo primeiro desafio resolvido

--- aula-js2-2.js
+++ aula-js2-2.js
@@ -1,7 +1,14 @@
 // Escreva uma função que receba uma array como parâmetro e devolva outra, mas sem elementos repetidos.
 // const numerosVarios = [ 45, 765, 23, 76, 43, 65, 2345, 677, 5, 34, 22, 546, 77, 4355, 5667, 45, 56, 7, 4, 755, 45]
 
-function numerosVarios(arr)
+// function numerosVarios(arr){
+//     const novoArr[];
+//     for(let i = 0; i < arr.length; i++){
+//         for(let x = 0; x < arr.length; x++){
+
+//         }
+//     }
+// }
     
 
 /////////////////////
@@ -11,7 +18,11 @@ function numerosVarios(arr)
 
 const letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 
-letras
+let arr = [];
+for(i = lentras.length-1; i >-1; i--){
+    arr.push(letras[i])
+}
+console.log(arr);
 
 /////////////////////
 
